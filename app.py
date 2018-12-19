2e60a1adb17ba1393cff4674b058aa899cd9ba86
Funkycoder <bsquan2009@gmail.com>
2018-12-19 17:58:17 +0700

    Using GET

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import feedparser
 from flask import Flask
+from flask import render_template
+from flask import request
+
 
 app = Flask(__name__)
 
@@ -9,21 +12,19 @@ RSS_FEEDS = {'cnn': 'http://rss.cnn.com/rss/edition.rss',
 
 
 @app.route('/')
-@app.route('/<publication>')
-def get_news(publication='cnn'):
+# @app.route('/<publication>')
+# def get_news(publication='cnn'):
+def get_news():
+    query = request.args.get("publication")
+    if not query or query.lower() not in RSS_FEEDS:
+        publication = "cnn"
+    else:
+        publication = query.lower()
     feed = feedparser.parse(RSS_FEEDS[publication])
     # feed['entries'] is a list
-    first_article = feed['entries'][0]
-    return """<html>
-        <body>
-            <h1>Headlines</h1>
-            <b>{0}</b></br>
-            <i>{1}</i></br>
-            <p>{2}</p></br>
-        </body>
-        </html>""".format(first_article.get("title"),
-                          first_article.get("published"),
-                          first_article.get("summary"))
+    # first_article = feed['entries'][0]
+    return render_template("home.html",
+                           articles=feed['entries'])
 
 
 if __name__ == '__main__':
